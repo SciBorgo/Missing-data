5590e97d8537acfdcff6ad937223d74cbb39f4da
Nicholas Tierney <nicholas.tierney@gmail.com>
2020-09-10 12:46:14 +1000

    update data munging from work meeting

--- R/read-gs.R
+++ R/read-gs.R
@@ -7,10 +7,10 @@ gs4_auth(email = "nicholas.tierney@monash.edu")
 sheet_url <- "https://docs.google.com/spreadsheets/d/1bxrBnQ8_ceX9Lz0-cRAB4VGBeW3SXluDQe-V84vDARM/edit"
 gs_football_r1 <- read_sheet(sheet_url, 
                              range = "A2:O201",
-                             col_types = "ciccccdcccccccc")
+                             col_types = "ciccccccccccccc")
 gs_football_r2_cols <- read_sheet(sheet_url, 
                                   range = "A2:H201",
-                                  col_types = "ciccccdc")
+                                  col_types = "cicccccc")
 gs_football_r2_results <- read_sheet(sheet_url, 
                                      range = "P2:V201",
                                      col_types = "ccccccc")
@@ -21,11 +21,23 @@ gs_football_r2 <- bind_cols(gs_football_r2_cols,
 gs_football <- bind_rows(reviewer_1 = gs_football_r1,
                          reviewer_2 = gs_football_r2,
                          .id = "reviewer") %>% 
-  clean_names()  %>% 
-  mutate(study_eligibility = as.logical(study_eligibility))
+  clean_names() %>% 
+  mutate(study_eligibility = as.logical(as.numeric(study_eligibility)),
+         exclusion_reason = replace_na(exclusion_reason, "Included"))
 
 library(visdat)
-vis_dat(gs_football, sort_type = FALSE)  
+vis_dat(gs_football_r1, sort_type = FALSE)
+vis_dat(gs_football_r2, sort_type = FALSE)
+vis_dat(gs_football, sort_type = FALSE)
+
+gs_football %>% 
+  filter(study_eligibility) %>% 
+  vis_dat(sort_type = FALSE)  
+
+gs_football_r1 %>% 
+  filter(study_eligibility == "1") %>% 
+  vis_dat(sort_type = FALSE)  
+
 
 # preliminary study
 gs_football %>% 
